86a5d1f22c3d522b2aab69dd3841ece555e1b3c8
Benjamin Saint Elien <shadowboxer79@hotmail.com>
2025-10-28 15:17:11 -0400

    Configuration file

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+const build = require('@bahmutov/cypress-esbuild-preprocessor')
+const addCucumberpreprocessor = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
+const createEsbuild = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      const builder = build({
+        plugins: [createEsbuild(config)]
+      })
+      on('file:preprocessor', builder)
+      addCucumberpreprocessor(on, config)
+      return config
+    },
+    specPattern: "**/*.feature"
+  },
+});
