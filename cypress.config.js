f3cc3bcd119a26da09daf6bef1953321c08c6790
Benjamin Saint Elien <shadowboxer79@hotmail.com>
2025-10-29 12:16:29 -0400

    Configurations

--- cypress.config.js
+++ cypress.config.js
@@ -14,6 +14,6 @@ module.exports = defineConfig({
       return config
     },
     specPattern: "**/*.feature",
-    supportFile: "cypress/e2e/**/*"
+//    supportFile: "cypress/e2e/**/*"
   },
 });
