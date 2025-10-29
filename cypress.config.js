d86b90138a2b873329904beb95e12be31db023ce
Benjamin Saint Elien <shadowboxer79@hotmail.com>
2025-10-29 15:24:34 -0400

    Configurations

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
       addCucumberpreprocessor(on, config)
       return config
     },
-    specPattern: 'cypress/**/*.cy.js',
+    specPattern: '**/*.feature',
     supportFile: false
   },
 });
